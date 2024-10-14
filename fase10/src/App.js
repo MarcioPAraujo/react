4867384f26ba1bf51f9efc8b42fe935239e60e1c
MarcioPAraujo <marciopereiraaraujo1997d1@gmail.com>
2024-10-14 19:45:13 -0300

    timer

--- fase10/src/App.js
+++ fase10/src/App.js
@@ -1,9 +1,21 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import UserForm_ShortVersion from "./UserForm_ShortVersion";
+import "./App.css"; // Import the CSS file
 
 function App() {
+  const [currentTime, setCurrentTime] = useState(new Date().toLocaleTimeString());
+
+  useEffect(() => {
+    const timer = setInterval(() => {
+      setCurrentTime(new Date().toLocaleTimeString());
+    }, 1000);
+
+    return () => clearInterval(timer); // Cleanup interval on component unmount
+  }, []);
+
   return (
     <div className="App">
+      <div className="watch">time: {currentTime}</div>
       <UserForm_ShortVersion />
     </div>
   );
